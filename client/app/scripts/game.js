b12be9dbbb0487ebbf89f1cc96ed35879073580f
Thomas Loubiou <xabufr@gmail.com>
2014-09-23 12:29:14 +0200

    Découplage game de l'initialisation THREEJS

--- client/app/scripts/game.js
+++ client/app/scripts/game.js
@@ -1,23 +1,10 @@
-define(['three', 'game/spacebox'], function(THREE, spacebox) {
+define(['game/render', 'game/spacebox'], function(render, spacebox) {
     'use strict';
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000, 1.0);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMapEnabled = true;
-
-    scene.add(spacebox);
-
-    var render = function() {
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-    };
+    render.scene.add(spacebox);
 
     return {
         start: function() {
-            document.body.appendChild(renderer.domElement);
-            render();
+            render.start();
         }
     }
 });
