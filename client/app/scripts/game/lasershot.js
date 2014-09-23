b76ee3147bb24c596a0f4b57429cfb4ec691f3a2
Thomas Loubiou <xabufr@gmail.com>
2014-09-23 20:29:49 +0200

    Refacto laser

--- client/app/scripts/game/lasershot.js
+++ client/app/scripts/game/lasershot.js
@@ -1,11 +1,17 @@
-define(['./laser/basiclaser'], function(laser) {
+define(['./laser/basiclaser'], function(BasicLaser) {
     return function(weapon, scene, maxLength) {
         weapon.mesh.updateMatrixWorld(true);
         var matrixWorld = weapon.mesh.matrixWorld.clone();
+
+        this.laser = new BasicLaser();
+        this.laser.mesh.rotation.setFromRotationMatrix(matrixWorld);
+        this.laser.mesh.position.set(0,0,0);
+
         var raycaster = new THREE.Raycaster();
         raycaster.ray.origin.setFromMatrixPosition(matrixWorld);
         matrixWorld.setPosition(new THREE.Vector3(0,0,0));
         raycaster.ray.direction = new THREE.Vector3(1,0,0).applyMatrix4(matrixWorld).normalize();
+        raycaster.far = maxLength;
         var intersects = raycaster.intersectObjects(scene.children, true);
         var scale = maxLength;
         var percute = false;
@@ -14,14 +20,11 @@ define(['./laser/basiclaser'], function(laser) {
             var distance = position.distanceTo(weapon.mesh.position);
             if(distance < maxLength) {
                 scale = distance;
-                percute = true;
+                percute = intersects[0].object;
             }
         }
-        this.laser = new laser();
         this.laser.mesh.scale.x = scale;
-        this.laser.mesh.position.set(0,0,0);
         weapon.mesh.localToWorld(this.laser.mesh.position);
-        this.laser.mesh.rotation.setFromRotationMatrix(weapon.mesh.matrixWorld);
         scene.add(this.laser.mesh);
     }
 });
