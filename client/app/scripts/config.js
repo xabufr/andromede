e737ed4b9211889e3dde285abe8c332622007703
Thomas Loubiou <xabufr@gmail.com>
2014-09-24 11:32:21 +0200

    Ajout du son du tir

--- client/app/scripts/config.js
+++ client/app/scripts/config.js
@@ -1,7 +1,8 @@
 require.config({
     baseUrl: "scripts",
     paths:{
-        "three": "../../bower_components/three.js/three"
+        "three": "../../bower_components/three.js/three",
+        "Howler": "../../bower_components/howler.js/howler"
     },
     shim: {
         "three": {
--- client/app/scripts/game/weapon.js
+++ client/app/scripts/game/weapon.js
@@ -1,4 +1,4 @@
-define(['three', './laser/lasershot'], function(THREE, LaserShot) {
+define(['three', './laser/lasershot', 'Howler'], function(THREE, LaserShot, Howler) {
     'use strict';
     return function Weapon(parent, core, laserPool) {
         var that = this;
@@ -8,6 +8,9 @@ define(['three', './laser/lasershot'], function(THREE, LaserShot) {
         this.tirer = function() {
             var laser = laserPool.get();
             laser.init(that, 2, 0.5);
+            var sound = new Howler.Howl({urls: ['assets/sound/laser.mp3']});
+            sound.play();
         };
+        this.isFiring = false;
     };
 });
