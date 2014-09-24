663a63c6d6066131505db339e4ac65f53f53a05c
zequiel <aramel@epsi.fr>
2014-09-25 00:24:13 +0200

    Add a none action area for cursor

--- client/app/scripts/game/cursor.js
+++ client/app/scripts/game/cursor.js
@@ -1,11 +1,23 @@
-define([], function (){
+define(['three'], function (THREE){
    return function Cursor () {
-        var that = this;
         this.sprite = document.getElementById("cursor");
+        this.minDistance = 40;
         this.move = function(render) {
             var input = render.input;
-            that.sprite.style.left = (input.mouse.abs.x - (32 / 2))+'px';
-            that.sprite.style.top = (input.mouse.abs.y - (32 / 2))+'px';
-     }
+            this.sprite.style.left = (input.mouse.abs.x - (32 / 2))+'px';
+            this.sprite.style.top = (input.mouse.abs.y - (32 / 2))+'px';
+        }.bind(this);
+
+        this.isInNoneActionArea = function() {
+            var cursorPosition = new THREE.Vector2(this.pixelToInt(this.sprite.style.left) + (32 / 2),
+                this.pixelToInt(this.sprite.style.top) + (32 / 2));
+            var centerPosition = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+            var distance = cursorPosition.distanceTo(centerPosition);
+            return distance >= -this.minDistance && distance <= this.minDistance;
+        }.bind(this);
+
+        this.pixelToInt = function(value) {
+          return parseInt(value.slice(0, value.length-2));
+        };
    };
 });
