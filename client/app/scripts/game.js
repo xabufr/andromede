1d4b8416733107ae81f73ba587d817877502f784
zequiel <aramel@epsi.fr>
2014-09-26 15:18:30 +0200

    Assign weapons to bones spaceShit, and engine particle to engine bone

--- client/app/scripts/game.js
+++ client/app/scripts/game.js
@@ -7,25 +7,16 @@ define(['core/core', 'game/camera', 'game/cursor', 'game/spacebox', 'game/spaceS
             Core.start(function(){
                 var spacebox = new Spacebox(Core);
 
-                var weapon = new Weapon(Core.objectsNode, Core, this.laserPool);
-                weapon.mesh.position.x = 6;
-                var weapon2 = new Weapon(Core.objectsNode, Core, this.laserPool);
-                weapon2.mesh.position.set(0, 10,0);
-                var weapon2 = new Weapon(Core.objectsNode, Core, this.laserPool);
-                weapon2.mesh.position.set(10, 0,0);
-                Core.frameListeners.push(function(_, delta) {
-                    weapon.isFiring = Core.input.mouse.buttons.left;
-                    weapon.update(_, delta);
-                });
-
-                var sunLight = new THREE.PointLight(0xffffff, 1.0, 50);
-                sunLight.position.set(20, 20, 0);
+                var sunLight = new THREE.PointLight(0xffffff, 5.0, 50);
+                sunLight.position.set(10, 10, 0);
                 sunLight.shadowCameraFar = 10;
                 sunLight.shadowCameraNear = 50;
 
                 Core.scene.add(sunLight);
 
                 var spaceShip = new SpaceShip(Core);
+                spaceShip.setWeapon(new Weapon(Core, this.laserPool));
+                spaceShip.setWeapon(new Weapon(Core, this.laserPool));
                 Core.camera.setTarget(spaceShip.mesh);
 
                 Core.cursor = new Cursor(Core.scene);
