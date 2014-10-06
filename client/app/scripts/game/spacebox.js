b2ad7809f6b341f8a635cb3bcd6ba7b49c62382e
Thomas Loubiou <xabufr@gmail.com>
2014-10-06 12:38:24 +0200

    Suppression code mort spacebox, Utilisation resource manager pour spacebox

--- client/app/scripts/game/spacebox.js
+++ client/app/scripts/game/spacebox.js
@@ -1,23 +1,11 @@
 define(['three'], function(THREE) {
     'use strict';
     return function(core) {
-        var urls = [
-            'assets/textures/spacebox/right1.png',
-            'assets/textures/spacebox/left2.png',
-            'assets/textures/spacebox/top3.png',
-            'assets/textures/spacebox/bottom4.png',
-            'assets/textures/spacebox/front5.png',
-            'assets/textures/spacebox/back6.png'
-        ];
-        var cubeMap = new THREE.ImageUtils.loadTextureCube(urls);
-        cubeMap.format = THREE.RGBFormat;
-        var spaceShader = THREE.ShaderLib['cube'];
-        spaceShader.uniforms['tCube'].value = cubeMap;
         var geometry = new THREE.BoxGeometry(500, 500, 500);
         var materialArray = [];
         for(var i=0;i<6;++i) {
             materialArray.push(new THREE.MeshBasicMaterial({
-                map: THREE.ImageUtils.loadTexture(urls[i]),
+                map: core.assetsLoader.get('textures', 'spacebox')[i],
                 side: THREE.BackSide,
                 depthWrite: false
             }));
