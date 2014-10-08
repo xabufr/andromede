d1e4f2a0d06a3f9a923083da289b0ed83e2e7576
zequiel <aramel@epsi.fr>
2014-10-08 23:38:59 +0200

    Create a red cursor

--- client/app/scripts/game/cursor.js
+++ client/app/scripts/game/cursor.js
@@ -2,10 +2,21 @@ define([], function (){
     'use strict';
    return function Cursor () {
         this.sprite = document.getElementById("cursor");
+        this.color = 'green';
+        this.textures = {
+            'red': 'assets/textures/cursorRed.png',
+            'green': 'assets/textures/cursorGreen.png'
+        }
         this.move = function(core) {
             var input = core.input;
             this.sprite.style.left = (input.mouse.abs.x - (32 / 2))+'px';
             this.sprite.style.top = (input.mouse.abs.y - (32 / 2))+'px';
         }.bind(this);
+
+        this.changeColor = function() {
+            var element = document.getElementById('cursor');
+            this.color = (this.color === 'green')?'red':'green';
+            element.setAttribute('href', this.textures[this.color]);
+        }
    };
 });
